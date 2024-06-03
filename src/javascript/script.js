3bbd9c6ea0d16a49fcf4ff8bd02ae7cca954a93c
wesley <wesleyaraujoteixeira98@gmail.com>
2024-06-03 19:49:21 -0300

    adding the files

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,12 +1,28 @@
 const menu = document.getElementById('mobile_btn');
 const content = document.getElementById('mobile_menu');
+const icon = document.getElementById('icon');
 
 const toggleMenu = () => {
     if(content.style.display == 'flex') {
-        content.style.display = 'none';
+        content.style.display = 'none';       
+        putMenu();
     }else{
         content.style.display = 'flex';
+        DeleteMenu();
+     
     }
 }
+
+
+const putMenu = () => {
+    icon.classList.remove('fa-x');
+    icon.classList.add('fa-solid fa-bars');
+}
+
+const DeleteMenu = () => {
+    icon.classList.add('fa-x');
+    icon.classList.remove('fa-solid fa-bars');
+}
+
 
 menu.addEventListener('click', toggleMenu);
